126964ea10ead9e0ffbaf90391f5d3fca33af702
Denis <65501295+sDeNySoKs@users.noreply.github.com>
2023-07-03 10:18:08 +0300

    Add files via upload

---
+++ laba1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);// Додаємо українську мову
+
+    int t1, t2, t3;
+    float total_time;
+    printf("Введіть три значення:\n");// Просимо ввести три значення
+    scanf("%d %d %d", &t1, &t2, &t3);// Зчитуємо значення t1, t2, t3
+    total_time = (float)(1.0 / t1) + (float)(1.0 / t2) + (float)(1.0 / t3);// Обчислюємо загальний час
+
+    printf("Час, необхідний для з'їдання пирога: %.2f годин\n", 1.0 / total_time);// Виводимо результат
+
+    return 0;
+}
+
